8944c40560e8f1513eea9ae9774974a4fffb544f
andreluis29 <andre.wenningkamp1@gmail.com>
2023-02-11 19:12:48 -0300

    Finalizado primeira parte da primeira aula e adicionado título ao README

--- manipulando-variaveis/user-input.py
+++ manipulando-variaveis/user-input.py
@@ -1,1 +1,27 @@
-print("Hello World")
+print("====================")
+print("Seja bem vindo ao jogo de adivinhação!")
+print("====================")
+
+numero_secreto = 42
+
+chute = int(input("Digite seu número: \n"))
+
+print("Você digitou: ", chute)
+
+if (numero_secreto == chute):
+    print("====================")
+    print("====================")
+    print("====================")
+    print("====================")
+    print("Você acertou o número secreto!\n\n")
+    print("====================")
+    print("====================")
+    print("PARABÉNS!")
+    print("====================")
+    print("====================")
+else:
+    print("====================")
+    print("====================")
+    print("====================")
+    print("====================")
+    print("Você errou!\n\n")
